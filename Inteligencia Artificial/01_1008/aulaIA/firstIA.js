b7694bba25e2a9c656b62f8f0b8275345bd1808c
Vinicius Raupp <101226503+Viniciusrcarlos@users.noreply.github.com>
2024-08-18 19:46:08 -0300

    ia

--- Inteligencia Artificial/01_1008/aulaIA/firstIA.js
+++ Inteligencia Artificial/01_1008/aulaIA/firstIA.js
@@ -5,11 +5,12 @@ let x41 = 1, x42 = 1, resultado4 = 1
 
 let p1 = -1, p2 = -1
 
-let soma, ajustes, quantidadeAjustesTotais
+let soma, ajustes, quantidadeAjustesTotais = 0
 let repeticoes = 0
 
 do {
     ajustes = 0
+
     soma = somar(x11, x12)
     y = transferencia(soma)
     if (y != resultado1) {
@@ -18,7 +19,40 @@ do {
         quantidadeAjustesTotais++
     }
 
+
+    soma = somar(x21, x22)
+    y = transferencia(soma)
+    if (y != resultado2) {
+        ajustar(x21, x22, resultado2, y)
+        ajustes++
+        quantidadeAjustesTotais++
+    }
+
+
+    soma = somar(x31, x32)
+    y = transferencia(soma)
+    if (y != resultado3) {
+        ajustar(x31, x32, resultado3, y)
+        ajustes++
+        quantidadeAjustesTotais++
+    }
+
+
+    soma = somar(x41, x42)
+    y = transferencia(soma)
+    if (y != resultado4) {
+        ajustar(x41, x42, resultado4, y)
+        ajustes++
+        quantidadeAjustesTotais++
+    }
+
+
 } while (ajustes != 0)
+
+console.log("Rede Neural");
+console.log("\nPeso 1: " + p1);
+console.log("\nPeso 2: " + p2);
+console.log("\nQuantidade de ajustes: " + quantidadeAjustesTotais);
 
 function somar(x1, x2) {
     return (x1*p1) + (x2*p2)
