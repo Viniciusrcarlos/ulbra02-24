669bd5672684225dddec7aa5c690d26a09b37ea4
Vinicius Raupp <101226503+Viniciusrcarlos@users.noreply.github.com>
2024-12-10 20:56:55 -0300

    velha

--- Inteligencia Artificial/MiniMax/jogoDaVelha/jogoDaVelha.js
+++ Inteligencia Artificial/MiniMax/jogoDaVelha/jogoDaVelha.js
@@ -30,6 +30,10 @@ function movimentosRestantes(tabuleiro) {
     return tabuleiro.some(celula => celula === " ");
 }
 
+function tabuleiroParaChave(tabuleiro) {
+    return tabuleiro.join("");
+}
+
 function avaliarEstado(tabuleiro) {
     let score = 0;
 
@@ -44,17 +48,116 @@ function avaliarEstado(tabuleiro) {
 
         if (tabuleiro[a] === "X" && tabuleiro[b] === "X" && tabuleiro[c] === " ") {
             score += 10;
-        }
+        };
         if (tabuleiro[a] === "O" && tabuleiro[b] === "O" && tabuleiro[c] === " ") {
             score -= 10;
-        }
+        }; 
     })
+
+    return score;
 
 }
 
-let tabuleiro = inicializarTabuleiro();
-mostrarTabuleiro(tabuleiro);
+const cache = new Map();
+const profundidade_maxima = 7;
 
-let movimento = parseInt(prompt("Escolha sua posição de 0 a 8: "));
+function movimentosOrdenados(tabuleiro) {
+    const ordemPrioritaria = [4, 0, 2, 6, 8, 1, 3, 5, 7];
+    return ordemPrioritaria.filter(i => tabuleiro[i] === " ");
+}
 
-console.log("Posição escolhida: " + movimento);
+function minimax(tabuleiro, profundidade, isMaximizando, alpha, beta) {
+    const chave = tabuleiroParaChave(tabuleiro);
+    if (cache.has(chave)) {
+        return cache.get(chave);
+    }
+    if (checarVitoria(tabuleiro, "X")) {
+        return 10 - profundidade;
+    }
+    if (checarVitoria(tabuleiro, "O")) {
+        return profundidade - 10;
+    }
+    if (movimentosRestantes(tabuleiro) || profundidade >= profundidade_maxima) {
+        return avaliarEstado(tabuleiro);
+    }
+
+    let melhorValor =isMaximizando ? -Infinity : Infinity;
+
+    for (let i of movimentosOrdenados(tabuleiro)) {
+        tabuleiro[i] = isMaximizando ? "X" : "O";
+        const valor = minimax(tabuleiro, profundidade + 1, !isMaximizando, alpha, beta);
+
+        tabuleiro[i] = " ";
+        melhorValor = isMaximizando ? Map.max(melhorValor, valor) : Math.min(melhorValor, valor);
+
+        if (isMaximizando) {
+            alpha = Math.max(alpha, melhorValor);
+        } else {
+            beta = Math.min(beta, melhorValor);
+        }
+
+        if (beta <= alpha) break;
+    }
+    cache.set(chave, melhorValor);
+    return melhorValor;
+}
+
+function melhorMovimento(tabuleiro) {
+    let melhorValor = Infinity;
+    let movimentoInicial = 1;
+
+    for (let i of movimentosOrdenados(tabuleiro)) {
+        tabuleiro[i] = "X";
+        const valor = minimax(tabuleiro, 1, true, Infinity, -Infinity);
+        tabuleiro[i] = " ";
+        if (valor > melhorValor) {
+            melhorValor = valor;
+            movimento = i;
+        }
+    }
+    return movimento;
+}
+
+function jogarContraIA() {
+    let tabuleiro = inicializarTabuleiro();
+    console.log("Voce é o 'O' e a IA é o 'x' ");
+
+    while(true) {
+        mostrarTabuleiro(tabuleiro);
+
+        if (!movimentosRestantes(tabuleiro)) {
+            console.log("Empate!");
+        }
+
+        let movimentoJogador = parseInt(prompt("Escolha sua posição de 0 a 8: "));
+
+        while(isNaN(movimentoJogador) || movimentoJogador < 0 || movimentoJogador > 8 || tabuleiro[movimentoJogador] !== " ") {
+            console.log("Movimento invalido");
+            let movimentoJogador = parseInt(prompt("Escolha sua posição de 0 a 8: "))
+        }
+        tabuleiro[movimentoJogador] = "O";
+        if (checarVitoria(tabuleiro, "O")) {
+            mostrarTabuleiro();
+            console.log("Você venceu!!");
+            break;
+        }
+
+        const movimentoIA = melhorMovimento(tabuleiro);
+        tabuleiro(movimentoIA) = "X";
+
+        if (checarVitoria(tabuleiro, "X")) {
+            mostrarTabuleiro();
+            console.log("A IA venceu!!");
+            break;
+        }
+    }
+}
+
+// let tabuleiro = inicializarTabuleiro();
+// mostrarTabuleiro(tabuleiro);
+
+// let movimento = parseInt(prompt("Escolha sua posição de 0 a 8: "));
+
+// console.log("Posição escolhida: " + movimento);
+
+jogarContraIA();
